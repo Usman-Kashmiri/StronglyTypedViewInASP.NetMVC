a86a9aa51f726b0225f2c1f7f4df0c64ef0571e7
Usman-Kashmiri <usmankashmiri378@gmail.com>
2022-04-08 08:42:33 +0500

    Add project files.

---
+++ StronglyTypedView/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using StronglyTypedView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTypedView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employees emp = new Employees();
+
+            emp.Id = 1;
+            emp.Name = "Usman";
+            emp.Designation = "employee";
+
+            Employees emp2 = new Employees();
+
+            emp2.Id = 1;
+            emp2.Name = "Usman";
+            emp2.Designation = "employee";
+
+            Employees emp3 = new Employees();
+
+            emp3.Id = 1;
+            emp3.Name = "Usman";
+            emp3.Designation = "employee";
+
+            //ViewBag.empdata = emp;
+            //ViewData["empdata"] = emp;
+
+            List<Employees> employeeslist = new List<Employees>();
+            employeeslist.Add(emp);
+            employeeslist.Add(emp2);
+            employeeslist.Add(emp3);
+
+            return View(employeeslist);
+        }
+    }
+}
